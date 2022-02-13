a36e5a7d3dad30e128d0ca13e2bc209b8a403698
wisang <wseom7@gmail.com>
2022-02-13 19:54:33 +0900

    timer added

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import requests
 from datetime import datetime
 import smtplib
+import time
 
 MY_LAT = 37.566536 # Your latitude
 MY_LONG = 126.977966 # Your longitude
 
 my_email = "testwisangeom@gmail.com"
 my_password = "eomm9409"
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
 
@@ -24,18 +18,25 @@ parameters = {
     "formatted": 0,
 }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
 
 def is_close():
-    return  abs(MY_LAT - iss_latitude) < 5.0 and abs(MY_LONG - iss_longitude) < 5.0
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    return abs(MY_LAT - iss_latitude) < 5.0 and abs(MY_LONG - iss_longitude) < 5.0
+
 
 def is_dark():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now()
+
     return time_now.hour > sunset
 
 
@@ -49,8 +50,10 @@ def send_mail(mail_body):
             msg=f"Subject:Python Exercise\n\n{mail_body}")
 
 
-if is_dark() and is_close():
-    send_mail("Look up!")
+while True:
+    time.sleep(60)
+    if is_dark() and is_close():
+        send_mail("Look up!")
 
 
 
