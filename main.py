22523815f54bba653ab8876d1c82ab1a22c47167
wisang <wseom7@gmail.com>
2022-02-13 19:36:53 +0900

    completed

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 from datetime import datetime
+import smtplib
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 37.566536 # Your latitude
+MY_LONG = 126.977966 # Your longitude
+
+my_email = "testwisangeom@gmail.com"
+my_password = "eomm9409"
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -28,10 +32,25 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+def is_close():
+    return  abs(MY_LAT - iss_latitude) < 5.0 and abs(MY_LONG - iss_longitude) < 5.0
+
+def is_dark():
+    return time_now.hour > sunset
+
+
+def send_mail(mail_body):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr="testwisangeom@gmail.com",
+            to_addrs="testwisangeom@yahoo.com",
+            msg=f"Subject:Python Exercise\n\n{mail_body}")
+
+
+if is_dark() and is_close():
+    send_mail("Look up!")
 
 
 
